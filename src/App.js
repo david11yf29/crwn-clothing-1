840caf97c98149563e01de729f6692b7d6411bf4
david11yf29 <david11yf29@gmail.com>
2020-07-24 23:11:47 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
+          }, () => {
+            console.log(this.state);
           })
         })
       } else {
